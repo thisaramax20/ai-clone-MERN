a29ba486e1520444acca0db2b8788ec550426bc4
thisaramax20 <thisarathejana20@gmail.com>
2024-12-27 17:36:12 +0530

    basic routes done in react

--- client/ai clone/src/main.jsx
+++ client/ai clone/src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Homepage from "./routes/homepage/Homepage.jsx";
+import Dashboard from "./routes/dashboard/Dashboard.jsx";
+import ChatPage from "./routes/chatPage/ChatPage.jsx";
 
-const router = createBrowserRouter([{ path: "/", element: <Homepage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Homepage /> },
+  {
+    path: "/dashboard",
+    children: [
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/dashboard/chats/:id", element: <ChatPage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
