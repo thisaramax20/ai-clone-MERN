6c01ce9a0461e596442d56bfa96d8c90d0327eb2
thisaramax20 <thisarathejana20@gmail.com>
2024-12-30 08:24:56 +0530

    sign in page done

--- client/ai clone/src/routes/signInPage/SignIn.jsx
+++ client/ai clone/src/routes/signInPage/SignIn.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-indigo-500 to-purple-600 py-16">
       <SignIn />
     </div>
   );
